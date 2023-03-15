9b603af657be2531c2c29b16985d917470f43ff4
Yanuar Fa <yanuarfatur41@gmail.com>
2023-03-15 19:52:41 +0700

    completed challange

--- main.go
+++ main.go
@@ -3,24 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	const i int = 21
-	const j bool = true
-	ya := 'Я'
-	var k float64 = 123.456
+	kalimat := "selamat malam"
+	jumlah := make(map[string]int)
 
-	fmt.Printf("%d \n", i)
-	fmt.Printf("%T \n", i)
-	fmt.Printf("%% \n")
-	fmt.Printf("%t \n", j)
-	fmt.Printf("\n")
-	fmt.Printf("%c \n", ya)
-	fmt.Printf("%d \n", 21)
-	fmt.Printf("%o \n", 21)
-	fmt.Printf("%x \n", 15)
-	fmt.Printf("%X \n", 3859)
-	fmt.Printf("%U \n", ya)
-	fmt.Printf("\n")
-	fmt.Printf("%f \n", k)
-	fmt.Printf("%e \n", k)
+	for _, h := range kalimat {
+		fmt.Printf("%c\n", h)
+		jumlah[string(h)] += 1
+	}
+
+	fmt.Print(jumlah)
 	fmt.Scanln()
 }
