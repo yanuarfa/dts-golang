ea7f750a3abb845d312a31dc6ac92bec04fc9c12
Yanuar Fa <yanuarfatur41@gmail.com>
2023-03-15 13:33:49 +0700

    updated

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const i int = 21
 	const j bool = true
-	ya := "\u042f"
+	ya := 'Я'
 	var k float64 = 123.456
 
 	fmt.Printf("%d \n", i)
@@ -13,12 +13,12 @@ func main() {
 	fmt.Printf("%% \n")
 	fmt.Printf("%t \n", j)
 	fmt.Printf("\n")
-	fmt.Printf("%q \n", ya)
-	fmt.Printf("%d \n", i)
-	fmt.Printf("%o \n", 25)
-	fmt.Printf("%x \n", "f")
-	fmt.Printf("%X \n", "F 13")
-	fmt.Printf("%+q \n", ya)
+	fmt.Printf("%c \n", ya)
+	fmt.Printf("%d \n", 21)
+	fmt.Printf("%o \n", 21)
+	fmt.Printf("%x \n", 15)
+	fmt.Printf("%X \n", 3859)
+	fmt.Printf("%U \n", ya)
 	fmt.Printf("\n")
 	fmt.Printf("%f \n", k)
 	fmt.Printf("%e \n", k)
